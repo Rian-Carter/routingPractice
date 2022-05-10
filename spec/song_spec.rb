fcd268cd1cffc5972c6d5738a7e91279a58ab954
Rian Carter <R.Carter86@hotmail.com>
2022-05-09 21:09:59 -0700

    all tests passing

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,6 +1,6 @@
 require('spec_helper')
 
-describe '#Song' do
+# describe '#Song' do
 
 
 
@@ -95,25 +95,27 @@ describe '#Song' do
     end
   end
 
-  # describe('.find_by_album') do
-  #   it("finds songs for an album") do
-  #     album2 = Album.new("Blue", nil)
-  #     album2.save
-  #     song = Song.new("Naima", @album.id, nil)
-  #     song.save()
-  #     song2 = Song.new("California", album2.id, nil)
-  #     song2.save()
-  #     expect(Song.find_by_album(album2.id)).to(eq([song2]))
-  #   end
-  # end
+  describe('.find_by_album') do
+    it("finds songs for an album") do
+      album = Album.new({:name => "Blue", :id => nil})
+      album.save()
+      album2 = Album.new({:name => "Green", :id => nil})
+      album2.save()
+      song = Song.new({:name => "Naima", :album_id => album.id, :id => nil})
+      song.save()
+      song2 = Song.new({:name => "California", :album_id => album2.id, :id => nil})
+      song2.save()
+      expect(Song.find_by_album(album2.id)).to(eq([song2]))
+    end
+  end
 
-#   describe('#album') do
-#     it("finds the album a song belongs to") do
-#       album = Album.new({:name => "Pinkerton", :id => nil})
-#       album.save()
-#       song = Song.new({:name =>"Naima",:album_id => album.id,:id => nil})
-#       song.save()
-#       expect(song.album()).to(eq(album))
-#     end
-#   end
-end
+  describe('#album') do
+    it("finds the album a song belongs to") do
+      album = Album.new({:name => "Pinkerton", :id => nil})
+      album.save()
+      song = Song.new({:name => "Naima",:album_id => album.id, :id => nil})
+      song.save()
+      expect(song.album()).to(eq(album))
+    end
+  end
+# end
